815030210c7837f536bc87eed2f4be981ad77543
Ferronn <ferronn@ferronn.dev>
2022-04-05 01:35:14 +0000

    cleanup and add write_image

--- wowrender/magick.c
+++ wowrender/magick.c
@@ -20,8 +20,7 @@ static MagickWand *check_magick_wand(lua_State *L, int k) {
 }
 
 static int new_magick_wand(lua_State *L) {
-  MagickWand **p;
-  p = lua_newuserdata(L, sizeof(*p));
+  MagickWand **p = lua_newuserdata(L, sizeof(*p));
   luaL_getmetatable(L, magick_wand_meta_name);
   lua_setmetatable(L, -2);
   *p = NewMagickWand();
@@ -29,33 +28,37 @@ static int new_magick_wand(lua_State *L) {
 }
 
 static int get_image_format(lua_State *L) {
-  MagickWand *wand;
-  char *format;
-  wand = check_magick_wand(L, 1);
-  format = MagickGetImageFormat(wand);
+  MagickWand *wand = check_magick_wand(L, 1);
+  char *format = MagickGetImageFormat(wand);
   lua_pushstring(L, format);
   MagickRelinquishMemory(format);
   return 1;
 }
 
 static int get_image_height(lua_State *L) {
-  MagickWand *wand;
   lua_pushnumber(L, MagickGetImageHeight(check_magick_wand(L, 1)));
   return 1;
 }
 
 static int get_image_width(lua_State *L) {
-  MagickWand *wand;
   lua_pushnumber(L, MagickGetImageWidth(check_magick_wand(L, 1)));
   return 1;
 }
 
 static int read_image(lua_State *L) {
-  MagickWand *wand;
-  const char *data;
-  wand = check_magick_wand(L, 1);
-  data = luaL_checkstring(L, 2);
+  MagickWand *wand = check_magick_wand(L, 1);
+  const char *data = luaL_checkstring(L, 2);
   if (MagickReadImage(wand, data) != MagickTrue) {
+    return magick_error(L, wand);
+  }
+  lua_pushboolean(L, 1);
+  return 1;
+}
+
+static int write_image(lua_State *L) {
+  MagickWand *wand = check_magick_wand(L, 1);
+  const char *path = luaL_checkstring(L, 2);
+  if (MagickWriteImage(wand, path) != MagickTrue) {
     return magick_error(L, wand);
   }
   lua_pushboolean(L, 1);
@@ -67,6 +70,7 @@ static struct luaL_Reg magick_wand_index[] = {
     {"get_image_height", get_image_height},
     {"get_image_width", get_image_width},
     {"read_image", read_image},
+    {"write_image", write_image},
     {NULL, NULL},
 };
 
